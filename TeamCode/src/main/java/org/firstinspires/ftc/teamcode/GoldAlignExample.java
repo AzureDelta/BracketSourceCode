bcd1a16080f8fc2442a8b4b0380ada188fa10e62
Brandon M <AzureDe;ta5356@gmail.com>
2018-10-01 17:26:02 -0700

    Changed GoldAlign Example group.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoldAlignExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoldAlignExample.java
@@ -37,7 +37,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 
-@TeleOp(name="GoldAlign Example", group="DogeCV")
+@TeleOp(name="GoldAlign Example", group="TeleOp")
 
 public class GoldAlignExample extends OpMode
 {
