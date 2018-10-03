cda3608b786d23567f1c154584cad12ad5bf3de5
Brandon McClelland <AzureDelta5356@gmail.com>
2018-10-02 19:23:14 -0700

    POV TeleOp
    Managed to recover some of the driver controls from the old BS file, they're ported in now.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WestCoastDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WestCoastDrive.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.util.Range;
 
 //*In theory* this should also be compatible with tank drive.
 
@@ -21,6 +22,14 @@ public class WestCoastDrive extends LinearOpMode{
         robot.init(hardwareMap);
         //loads hardwareMap
 
+        double R;
+        double X;
+        double Y;
+        double FLvalue;
+        double FRvalue;
+        double RLvalue;
+        double RRvalue;
+
         // Send telemetry message to signify robot waiting;
         telemetry.addData("Say", "Shock drone going live!");    //
         telemetry.update();
@@ -32,10 +41,29 @@ public class WestCoastDrive extends LinearOpMode{
         while(opModeIsActive())
         {
             //tank drive code
-            robot.motorFR.setPower(gamepad1.right_stick_y);
-            robot.motorFL.setPower(gamepad1.left_stick_y);
-            robot.motorRR.setPower(gamepad1.right_stick_y);
-            robot.motorRL.setPower(gamepad1.left_stick_y);
+            // Run wheels in POV mode (note: The joystick goes negative when pushed forwards, so negate it)
+            // In this mode the Left stick moves the robot fwd and back, the Right stick turns left and right.
+            // This way it's also easy to just drive straight, or just turn.
+            R=gamepad1.right_stick_x;
+            X=gamepad1.left_stick_x;
+            Y=gamepad1.left_stick_y;
+
+            // Combine drive and turn for blended motion.
+            FLvalue =  - Y + X +R;
+            FRvalue =  + Y + X + R;
+            RLvalue =  - Y -X+ R;
+            RRvalue =  + Y -X + R;
+
+            //maxes the values at 1
+            FLvalue = Range.clip(FLvalue, -1, 1 );
+            FRvalue = Range.clip(FRvalue, -1, 1 );
+            RLvalue = Range.clip(RLvalue, -1, 1 );
+            RRvalue = Range.clip(RRvalue, -1, 1 );
+
+            robot.motorFL.setPower(FLvalue);
+            robot.motorFR.setPower(FRvalue);
+            robot.motorRL.setPower(RLvalue);
+            robot.motorRR.setPower(RRvalue);
 
             idle();
         }
