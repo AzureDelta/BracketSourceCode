939ae72633c68b22ef1eeee2077fe43df86e43b7
albracher <allenbracher141@gmail.com>
2018-11-17 20:41:36 -0800

    test auton created
    drive, strafe right, strafe left, rotate 135 cw, rotate 135 ccw

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuton.java
@@ -0,0 +1,225 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.disnodeteam.dogecv.CameraViewDisplay;
+import com.disnodeteam.dogecv.DogeCV;
+import com.disnodeteam.dogecv.detectors.roverrukus.GoldAlignDetector;
+import com.disnodeteam.dogecv.detectors.roverrukus.SamplingOrderDetector;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+@Autonomous(name="TestAuton", group="Auto")
+
+/* Declare OpMode members. */
+
+
+public class TestAuton extends LinearOpMode {
+
+    HardwareConfig robot = new HardwareConfig();
+
+    private GoldAlignDetector detector;
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+
+    static final double COUNTS_PER_MOTOR_REV = 1120;    // eg: Andymark Motor Encoder (40:1)
+    static final double DRIVE_GEAR_REDUCTION = 0.5;     // This is < 1.0 if geared UP
+    static final double WHEEL_DIAMETER_INCHES = 4.0;     // For figuring circumference
+    static final double COUNTS_PER_ROTATION = COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION;     //used to compute degrees
+    static final double INCHES = (COUNTS_PER_MOTOR_REV * 0.5) / (WHEEL_DIAMETER_INCHES * Math.PI); //calculates counts per inch
+    static final double FEET = 12 * INCHES; //calculates counts per foot
+    static final double DEGREES = (1120) / 360; //calculates counts per degree
+    public static final double M = (2 / Math.sqrt(2));
+    public static final double ARM_SPEED = 0.1;
+    public static final double DRIVE_SPEED = 0.5;
+    /*
+    660 counts of encoder = 4 inches
+    1 inch = 165 counts
+    */
+
+    @Override
+
+    public void runOpMode() {
+        robot.init(hardwareMap);
+
+        //send telemetry
+        telemetry.addData("Status", "Ready to run Test Autonomous");
+        telemetry.update();
+
+        //it'll wait 2.5 seconds
+        sleep(2500);
+        //drive, strafe, turn
+        drive(0.5, 1.5 * INCHES);
+        sleep(1000);
+        //positive dist. is right
+        //negative dist. is left
+        //first right 2 inches
+        strafe(0.5, 2 * INCHES);
+        sleep(1000);
+        //then left 2 inches
+        strafe(0.5, -2 * INCHES);
+        sleep(1000);
+        //turn clockwise 135 deg?
+        turn(0.5, 135);
+        sleep(1000);
+        //them turn CCW 135 deg
+        turn(0.5, -135);
+        sleep(1000);
+    }
+
+
+    public void drive(double speed, double distance) {
+        //declares target point storage variables
+        int targetFL;
+        int targetFR;
+        int targetRL;
+        int targetRR;
+
+        // Determine new target position, and pass to motor controller
+        targetFL = robot.motorFL.getCurrentPosition() + (int) (distance);
+        targetFR = robot.motorFR.getCurrentPosition() + (int) (distance);
+        targetRL = robot.motorRL.getCurrentPosition() + (int) (distance);
+        targetRR = robot.motorRR.getCurrentPosition() + (int) (distance);
+        robot.motorFL.setTargetPosition(targetFL);
+        robot.motorFR.setTargetPosition(targetFR);
+        robot.motorRL.setTargetPosition(targetRL);
+        robot.motorRR.setTargetPosition(targetRR);
+
+        // Turn On RUN_TO_POSITION
+        robot.motorFL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.motorFR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.motorRL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.motorRR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        // reset the timeout time and start motion.
+        robot.motorFL.setPower(Math.abs(speed));
+        robot.motorFR.setPower(Math.abs(speed));
+        robot.motorRL.setPower(Math.abs(speed));
+        robot.motorRR.setPower(Math.abs(speed));
+
+        // keep looping while we are still active, and there is time left, and both motors are running.
+        // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+        // its target position, the motion will stop.  This is "safer" in the event that the robot will
+        // always end the motion as soon as possible.
+        // However, if you require that BOTH motors have finished their moves before the robot continues
+        // onto the next step, use (isBusy() || isBusy()) in the loop test.
+
+        // Stop all motion;
+        robot.motorFL.setPower(0);
+        robot.motorFR.setPower(0);
+        robot.motorRL.setPower(0);
+        robot.motorRR.setPower(0);
+
+        // Turn off RUN_TO_POSITION
+        robot.motorFL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.motorFR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.motorRL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.motorRR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
+
+    public void turn(double speed, double angle) {
+        //declares target point storage variables
+        int targetFL;
+        int targetFR;
+        int targetRL;
+        int targetRR;
+
+        //rotates counter clockwise based on angle
+        targetFL = robot.motorFL.getCurrentPosition() + (int) (COUNTS_PER_ROTATION - angle);
+        targetFR = robot.motorFR.getCurrentPosition() + (int) (COUNTS_PER_ROTATION + angle);
+        targetRL = robot.motorRL.getCurrentPosition() + (int) (COUNTS_PER_ROTATION - angle);
+        targetRR = robot.motorRR.getCurrentPosition() + (int) (COUNTS_PER_ROTATION + angle);
+        robot.motorFL.setTargetPosition(targetFL);
+        robot.motorFR.setTargetPosition(targetFR);
+        robot.motorRL.setTargetPosition(targetRL);
+        robot.motorRR.setTargetPosition(targetRR);
+
+        // Turn On RUN_TO_POSITION
+        robot.motorFL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.motorFR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.motorRL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.motorRR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        // reset the timeout time and start motion.
+        robot.motorFL.setPower(Math.abs(speed));
+        robot.motorFR.setPower(Math.abs(speed));
+        robot.motorRL.setPower(Math.abs(speed));
+        robot.motorRR.setPower(Math.abs(speed));
+
+        // keep looping while we are still active, and there is time left, and both motors are running.
+        // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+        // its target position, the motion will stop.  This is "safer" in the event that the robot will
+        // always end the motion as soon as possible.
+        // However, if you require that BOTH motors have finished their moves before the robot continues
+        // onto the next step, use (isBusy() || isBusy()) in the loop test.
+
+        // Stop all motion;
+        robot.motorFL.setPower(0);
+        robot.motorFR.setPower(0);
+        robot.motorRL.setPower(0);
+        robot.motorRR.setPower(0);
+
+        // Turn off RUN_TO_POSITION
+        robot.motorFL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.motorFR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.motorRL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.motorRR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public void strafe(double speed, double distance) {
+        //declares target point storage variables
+        int targetFL;
+        int targetFR;
+        int targetRL;
+        int targetRR;
+
+        // Determine new target position, and pass to motor controller
+        targetFL = robot.motorFL.getCurrentPosition() + (int) (distance);
+        targetFR = robot.motorFR.getCurrentPosition() + (int) (-distance);
+        targetRL = robot.motorRL.getCurrentPosition() + (int) (-distance);
+        targetRR = robot.motorRR.getCurrentPosition() + (int) (distance);
+        robot.motorFL.setTargetPosition(targetFL);
+        robot.motorFR.setTargetPosition(targetFR);
+        robot.motorRL.setTargetPosition(targetRL);
+        robot.motorRR.setTargetPosition(targetRR);
+
+        // Turn On RUN_TO_POSITION
+        robot.motorFL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.motorFR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.motorRL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.motorRR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        // reset the timeout time and start motion.
+
+        if(opModeIsActive()) {
+            robot.motorFL.setPower(Math.abs(speed));
+            robot.motorFR.setPower(Math.abs(speed));
+            robot.motorRL.setPower(Math.abs(speed));
+            robot.motorRR.setPower(Math.abs(speed));
+
+            // keep looping while we are still active, and there is time left, and both motors are running.
+            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+            // its target position, the motion will stop.  This is "safer" in the event that the robot will
+            // always end the motion as soon as possible.
+            // However, if you require that BOTH motors have finished their moves before the robot continues
+            // onto the next step, use (isBusy() || isBusy()) in the loop test.
+
+            // Stop all motion;
+            robot.motorFL.setPower(0);
+            robot.motorFR.setPower(0);
+            robot.motorRL.setPower(0);
+            robot.motorRR.setPower(0);
+        }
+
+        // Turn off RUN_TO_POSITION
+        robot.motorFL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.motorFR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.motorRL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.motorRR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
+}
