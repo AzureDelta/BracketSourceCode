8f3480387b2ba8bb2787297af7106ca14b59f6bf
albracher <allenbracher141@gmail.com>
2018-11-25 19:04:35 -0800

    code for linear actuator to function properly

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareConfigVeryLite.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+
+public class HardwareConfigVeryLite {
+    public DcMotor yellowJacket = null;
+
+    public static final double MID_SERVO = 0.5;//legacy code, can be removed
+    //public ColorSensor colorSensor;//legacy code, can be removed
+
+
+    /* local OpMode members. */
+    HardwareMap hwMap = null;
+    private ElapsedTime period = new ElapsedTime();
+
+    /* Constructor */
+    public HardwareConfigVeryLite() {
+
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+
+        // Define and Initialize Motors
+        yellowJacket = hwMap.get(DcMotor.class, "al");
+
+        yellowJacket.setDirection(DcMotor.Direction.FORWARD);//requires testing
+
+
+        // Set all motors to zero power
+        yellowJacket.setPower(0);
+
+        //set zero power behavior
+        yellowJacket.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        yellowJacket.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //Not using encoders for non drive train to allow for more direct control of power.
+        //Arm uses encoders to make sure motors stay in sync
+        //same with intake
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearActuatorTest.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.util.Range;
+import java.util.*;
+
+//*In theory* this should also be compatible with tank drive.
+
+@TeleOp(name ="Linear Actuator Test", group ="TeleOp")
+public class LinearActuatorTest extends LinearOpMode {
+    public static final double ARM_SPEED = 1;
+    //public static final double ARM_SPEED = 0.5;
+    public static final double INTAKE_SPEED = 0.5;
+
+    /* Declare OpMode members. */
+    HardwareConfigVeryLite robot           = new HardwareConfigVeryLite();   //Configs hardware
+
+
+    @Override
+    public void runOpMode () throws InterruptedException
+    {
+        robot.init(hardwareMap);
+
+        double actuatorPower;
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Shock drone going live!");
+        telemetry.update();
+
+        waitForStart();
+
+        telemetry.addData("Status", "ASSUMING DIRECT CONTROL");
+        telemetry.update();
+
+        while(opModeIsActive())
+        {
+
+            actuatorPower = (((gamepad1.right_trigger+gamepad2.right_trigger)+(0.1*-gamepad2.left_stick_y))-(gamepad1.left_trigger+gamepad2.left_trigger));
+            actuatorPower *= ARM_SPEED;
+
+            //sets maxes for each value
+            actuatorPower = Range.clip(actuatorPower, -ARM_SPEED, ARM_SPEED);
+
+            robot.yellowJacket.setPower(actuatorPower);
+        }
+
+    }
+}
