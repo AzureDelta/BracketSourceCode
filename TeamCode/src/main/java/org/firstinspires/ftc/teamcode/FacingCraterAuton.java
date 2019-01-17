1cb57d9eb0f651552d18687c159e56c3d9db9e53
Brandon McClelland <AzureDelta5356@gmail.com>
2019-01-16 22:34:06 -0800

    Revert "Revert "Moved Drive Methods to AutonMap""
    This reverts commit a741cd391f2b345197e8f4145afd5a1a579e7db5.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FacingCraterAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FacingCraterAuton.java
@@ -8,7 +8,7 @@ import com.disnodeteam.dogecv.CameraViewDisplay;
 import com.disnodeteam.dogecv.DogeCV;
 import com.disnodeteam.dogecv.detectors.roverrukus.GoldAlignDetector;
 
-@Autonomous(name = "OriginalCrater", group = "Auto")
+@Autonomous(name = "Full Crater", group = "Auto")
 
 /* Declare OpMode members. */
 
@@ -18,16 +18,6 @@ public class FacingCraterAuton extends LinearOpMode {
 
     private GoldAlignDetector detector;
     private ElapsedTime runtime = new ElapsedTime();
-
-    static final double COUNTS_PER_MOTOR_REV = 1120;    // eg: Andymark Motor Encoder (40:1)
-    static final double DRIVE_GEAR_REDUCTION = 0.5;     // This is < 1.0 if geared UP
-    static final double WHEEL_DIAMETER_INCHES = 4.0;     // For figuring circumference
-    static final double COUNTS_PER_ROTATION = COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION;     //used to compute degrees
-    static final double INCHES = (COUNTS_PER_MOTOR_REV * (80/120) / (WHEEL_DIAMETER_INCHES * Math.PI)); //calculates counts per inch
-    static final double FEET = 12 * INCHES;
-    double OFFSET = 0;
-    public static final double M = (2 / Math.sqrt(2));
-    public static final double DRIVE_SPEED = 0.5;
 
     @Override
 
@@ -69,15 +59,15 @@ public class FacingCraterAuton extends LinearOpMode {
         //lower the robot
         //actuate(1.0, 12.5);
         //detach arm
-        strafe(DRIVE_SPEED, 2 * INCHES * M);
+        robot.strafe(robot.DRIVE_SPEED, 2 * robot.INCHES * robot.M);
         //store arm
 /*
         actuate(-0.9, 10);
 */
         //reset position
-        drive(DRIVE_SPEED, 2 * INCHES * M);
+        robot.drive(robot.DRIVE_SPEED, 2 * robot.INCHES * robot.M);
         //detach arm
-        strafe(DRIVE_SPEED, -2 * INCHES * M);
+        robot.strafe(robot.DRIVE_SPEED, -2 * robot.INCHES * robot.M);
 
         //declare sentinel variable
         boolean runLoop = true;
@@ -90,13 +80,13 @@ public class FacingCraterAuton extends LinearOpMode {
 
         alignGold();
         if(!detector.isFound()){
-            strafe(DRIVE_SPEED, M * -17 * INCHES);
-            OFFSET-=170;
+            robot.strafe(robot.DRIVE_SPEED, robot.M * -17 * robot.INCHES);
+            robot.OFFSET-=170;
             alignGold();
         }
         if(!detector.isFound()){
-            strafe(DRIVE_SPEED, M * ((2*FEET) + (10 * INCHES)));
-            OFFSET+=340;
+            robot.strafe(robot.DRIVE_SPEED, robot.M * ((2*robot.FEET) + (10 * robot.INCHES)));
+            robot.OFFSET+=340;
             alignGold();
         }
 
@@ -110,7 +100,7 @@ public class FacingCraterAuton extends LinearOpMode {
             //ONE TILE IS 24 INCHES X 24 INCHES
 
             //drive to crater
-            drive(0.5, M * ((2*FEET) + (10 * INCHES)));
+            robot.drive(0.5, robot.M * ((2*robot.FEET) + (10 * robot.INCHES)));
 
         }
 
@@ -129,141 +119,23 @@ public class FacingCraterAuton extends LinearOpMode {
         robot.motorRL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         while (detector.getAligned() != true && runtime.seconds() < 20 && detector.isFound()) {
             if (detector.getXPosition() < 320 && detector.isFound()) {
-                robot.motorFL.setPower(-DRIVE_SPEED);
-                robot.motorFR.setPower(-DRIVE_SPEED);
-                robot.motorRL.setPower(DRIVE_SPEED);
-                robot.motorRR.setPower(DRIVE_SPEED);
-                OFFSET--;
+                robot.motorFL.setPower(-robot.DRIVE_SPEED);
+                robot.motorFR.setPower(-robot.DRIVE_SPEED);
+                robot.motorRL.setPower(robot.DRIVE_SPEED);
+                robot.motorRR.setPower(robot.DRIVE_SPEED);
+                robot.OFFSET--;
                 telemetry.addData("Status", "Target left.");
                 telemetry.update();
 
             } else if (detector.getXPosition() > 320 && detector.isFound()) {
-                robot.motorFL.setPower(DRIVE_SPEED);
-                robot.motorFR.setPower(DRIVE_SPEED);
-                robot.motorRL.setPower(-DRIVE_SPEED);
-                robot.motorRR.setPower(-DRIVE_SPEED);
-                OFFSET++;
+                robot.motorFL.setPower(robot.DRIVE_SPEED);
+                robot.motorFR.setPower(robot.DRIVE_SPEED);
+                robot.motorRL.setPower(-robot.DRIVE_SPEED);
+                robot.motorRR.setPower(-robot.DRIVE_SPEED);
+                robot.OFFSET++;
                 telemetry.addData("Status", "Target Right");
                 telemetry.update();
             }
         }
     }
-
-    public void drive(double speed, double distance) {
-        if (opModeIsActive()) {
-            // Resets encoder values so that it doesn't attempt to run to outdated values
-            robot.motorFL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            robot.motorFR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            robot.motorRL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            robot.motorRR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-            // Sets motors to run to a given encoder value
-            robot.motorFL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            robot.motorFR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            robot.motorRL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            robot.motorRR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-            // Declares target point storage variables
-            int targetFL;
-            int targetFR;
-            int targetRL;
-            int targetRR;
-
-            // Determines new target position, and pass to motor controller
-            targetFL = robot.motorFL.getCurrentPosition() + (int) (distance);
-            targetFR = robot.motorFR.getCurrentPosition() + (int) (distance);
-            targetRL = robot.motorRL.getCurrentPosition() + (int) (distance);
-            targetRR = robot.motorRR.getCurrentPosition() + (int) (distance);
-            robot.motorFL.setTargetPosition(targetFL);
-            robot.motorFR.setTargetPosition(targetFR);
-            robot.motorRL.setTargetPosition(targetRL);
-            robot.motorRR.setTargetPosition(targetRR);
-
-            // Motors are set to run at a certain speed until one reaches its target position
-            while (robot.motorFL.isBusy() && robot.motorFR.isBusy() && robot.motorRL.isBusy() && robot.motorRR.isBusy()) {
-                robot.motorFL.setPower(Math.abs(speed));
-                robot.motorFR.setPower(Math.abs(speed));
-                robot.motorRL.setPower(Math.abs(speed));
-                robot.motorRR.setPower(Math.abs(speed));
-                // keep looping while we are still active, and there is time left, and both motors are running.
-                // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
-                // its target position, the motion will stop.  This is "safer" in the event that the robot will
-                // always end the motion as soon as possible.
-                // However, if you require that BOTH motors have finished their moves before the robot continues
-            }
-        }
-        // The motors are shutdown when a motor gets to its target position
-        robot.motorFL.setPower(0);
-        robot.motorFR.setPower(0);
-        robot.motorRL.setPower(0);
-        robot.motorRR.setPower(0);
-    }
-
-    public void strafe(double speed, double distance) {
-        if (opModeIsActive()) {
-            // Resets encoder values so that it doesn't attempt to run to outdated values
-            robot.motorFL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            robot.motorFR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            robot.motorRL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            robot.motorRR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-            // Sets motors to run to a given encoder value
-            robot.motorFL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            robot.motorFR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            robot.motorRL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            robot.motorRR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-            // Declares target point storage variables
-            int targetFL;
-            int targetFR;
-            int targetRL;
-            int targetRR;
-
-            // Determines new target position, and pass to motor controller
-            targetFL = robot.motorFL.getCurrentPosition() + (int) (distance);
-            targetFR = robot.motorFR.getCurrentPosition() + (int) (distance);
-            targetRL = robot.motorRL.getCurrentPosition() + (int) (distance);
-            targetRR = robot.motorRR.getCurrentPosition() + (int) (distance);
-            robot.motorFL.setTargetPosition(targetFL);
-            robot.motorFR.setTargetPosition(targetFR);
-            robot.motorRL.setTargetPosition(targetRL);
-            robot.motorRR.setTargetPosition(targetRR);
-
-            // Motors are set to run at a certain speed until one reaches its target position
-            while (robot.motorFL.isBusy() && robot.motorFR.isBusy() && robot.motorRL.isBusy() && robot.motorRR.isBusy()) {
-                robot.motorFL.setPower(Math.abs(-speed));
-                robot.motorFR.setPower(Math.abs(speed));
-                robot.motorRL.setPower(Math.abs(speed));
-                robot.motorRR.setPower(Math.abs(-speed));
-                // keep looping while we are still active, and there is time left, and both motors are running.
-                // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
-                // its target position, the motion will stop.  This is "safer" in the event that the robot will
-                // always end the motion as soon as possible.
-                // However, if you require that BOTH motors have finished their moves before the robot continues
-            }
-        }
-        // The motors are shutdown when a motor gets to its target position
-        robot.motorFL.setPower(0);
-        robot.motorFR.setPower(0);
-        robot.motorRL.setPower(0);
-        robot.motorRR.setPower(0);
-    }
-
-/*    public void actuate(double speed, double time) {
-        runtime.reset();
-        while (opModeIsActive() && (runtime.seconds() < time)) {
-            telemetry.addData("Status:", "Actuating", runtime.seconds());
-            telemetry.update();
-            robot.actuator.setPower(speed);
-        }
-    }*/
-
-    /*public void intake(double speed, double time) {
-        runtime.reset();
-        while (opModeIsActive() && (runtime.seconds() < time)) {
-            telemetry.addData("Status:", "Actuating", runtime.seconds());
-            telemetry.update();
-            robot.intake.setPower(speed);
-        }
-    }*/
 }
