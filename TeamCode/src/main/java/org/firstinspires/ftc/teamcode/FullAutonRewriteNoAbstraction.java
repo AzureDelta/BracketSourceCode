107533d30b1a29bee9b0dc5e16ecd7a1eb4de043
Brandon M <AzureDelta5356@gmail.com>
2019-01-18 20:58:19 -0800

    no abstraction

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FullAutonRewriteNoAbstraction.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FullAutonRewriteNoAbstraction.java
@@ -1,11 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.ftccommon.SoundPlayer;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import com.disnodeteam.dogecv.detectors.roverrukus.GoldAlignDetector;
+
+import static org.firstinspires.ftc.teamcode.FullAutonomousRewriteTest.INCHES;
+import static org.firstinspires.ftc.teamcode.FullAutonomousRewriteTest.M;
 
 @Autonomous(name="FART NoAbstraction", group="Auto")
 
@@ -16,12 +15,6 @@ public class FullAutonRewriteNoAbstraction extends LinearOpMode {
 
     AutonMap robot = new AutonMap();
 
-    static final double COUNTS_PER_MOTOR_REV = 1120;    // eg: Andymark Motor Encoder (40:1)
-    static final double DRIVE_GEAR_REDUCTION = 80/120;     // This is < 1.0 if geared UP
-    static final double WHEEL_DIAMETER_INCHES = 4.0;     // For figuring circumference
-    static final double COUNTS_PER_ROTATION = COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION;     //used to compute degrees
-    static final double INCHES = (COUNTS_PER_ROTATION * 0.5) / (WHEEL_DIAMETER_INCHES * Math.PI); //calculates counts per inch
-    public static final double M = (2 / Math.sqrt(2));
     @Override
 
     public void runOpMode() {
@@ -32,10 +25,6 @@ public class FullAutonRewriteNoAbstraction extends LinearOpMode {
         telemetry.update();
 
         waitForStart();
-
-
-        // Play the audio
-        //SoundPlayer.getInstance().startPlaying(hardwareMap.appContext, soundID);
 
         //drive, strafe
         robot.drive(0.5, 12 * INCHES * M);
